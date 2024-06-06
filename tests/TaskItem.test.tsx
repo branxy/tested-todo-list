2399cfdaa03b04e191c0101de14c99a88da0105a
branxy <churkin.michael@gmail.com>
2024-06-06 18:52:47 +0300

    tests: added TaskItem test

---
+++ tests/TaskItem.test.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
+import TaskApp from "../src/tasks/TaskApp";
+
+import { describe, expect, it } from "vitest";
+
+describe("Task Item component functionality", () => {
+  const renderTaskApp = () => {
+    render(<TaskApp />);
+
+    return {
+      form: screen.getByRole("form"),
+      input: screen.getByPlaceholderText("New task name"),
+    };
+  };
+  it(`should mark task as "done" when its checkbox is toggled`, () => {
+    const { form, input } = renderTaskApp();
+    fireEvent.change(input, { target: { value: "New Task" } });
+    fireEvent.submit(form);
+
+    const checkbox = screen.getByRole("checkbox");
+    fireEvent.click(checkbox);
+
+    // navigate to "completed" tab
+    const completedTasksTab = screen.getByText("Completed");
+    fireEvent.click(completedTasksTab);
+
+    const checkedCheckbox = screen.getByRole("checkbox");
+    expect(checkedCheckbox).toBeInTheDocument();
+  });
+});
